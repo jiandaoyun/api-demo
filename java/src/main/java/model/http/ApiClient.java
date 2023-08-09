6795c18d34056cc30943d4e0d4b32e0dd3b1d57e
Mars.Du <Mars.Du@fanruan.com>
2023-08-09 20:44:12 +0800

    fix:FXD-50537 使用正确的判断方式处理返回为空的接口

--- java/src/main/java/model/http/ApiClient.java
+++ java/src/main/java/model/http/ApiClient.java
@@ -15,6 +15,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.io.EmptyInputStream;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.ssl.SSLContextBuilder;
 import util.LimitUtil;
@@ -139,8 +140,8 @@ public abstract class ApiClient {
         int statusCode = response.getStatusLine().getStatusCode();
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> result = new HashMap<>();
-        // 有部分接口直接返回 没有数据
-        if (response.getEntity().getContentLength() > 0) {
+        // 有部分接口直接返回 没有数据 fix：不能用content-length大于0判断，response header为gzip编码方式的情况下为-1
+        if (!(response.getEntity().getContent() instanceof EmptyInputStream)) {
             result = (Map<String, Object>) mapper.readValue(response.getEntity().getContent(), Object.class);
         }
         if (statusCode >= 400) {
@@ -168,7 +169,6 @@ public abstract class ApiClient {
         List<Header> headerList = new ArrayList<>();
         headerList.add(new BasicHeader("Authorization", "Bearer " + apiKey));
         headerList.add(new BasicHeader("Content-Type", "application/json;charset=utf-8"));
-        headerList.add(new BasicHeader("Accept-Encoding", "identity"));
         return headerList.toArray(new Header[headerList.size()]);
     }
 
@@ -193,7 +193,8 @@ public abstract class ApiClient {
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> result = new HashMap<>();
         // 有部分接口直接返回 没有数据
-        if (response.getEntity().getContentLength() > 0) {
+        // fix：不能用content-length大于0判断，response header为gzip编码方式的情况下为-1
+        if (!(response.getEntity().getContent() instanceof EmptyInputStream)) {
             result = (Map<String, Object>) mapper.readValue(response.getEntity().getContent(), Object.class);
         }
         if (statusCode >= 400) {
